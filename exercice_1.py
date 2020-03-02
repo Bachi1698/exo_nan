a0ff82c447e897c35feb5720b83c3af02124d0bf
elysee-01 <elyseekevin49@gmail.com>
2020-03-02 01:35:34 +0000

    Choix des exercices valide

--- exercice_1.py
+++
@@ -1,7 +0,0 @@
-t = int(input("entrez la température de l'eau:"))
-if t <= 48 :
-    print("glace")
-elif t>= 60:
-    print("vapeur")
-else:
-    print("vapeur")
--- exo3.py
+++ exo3.py
@@ -2,6 +2,6 @@ n=1
 liste=[1/n]
 a = float(input("entrez un nombre:"))
 while sum (liste) < a:
-    n = n+1
+    n += 1
     liste.append(1/n)
 print("le plus petit entier est:",n )
