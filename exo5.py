a1e8c2c28b0fd50b24b051f5e2ef1cf25c0b9b80
franck <franckassovie12345@gmail.com>
2020-03-04 04:15:19 +0000

    update exo5

--- exo5.py
+++ exo5.py
@@ -5,19 +5,21 @@ nombr_acc = int(input("Combien d'accident avez vous deja subit : "))
 
 tarif = "Aucun"
 
-
 if age < 25:
     if annee_per < 2:
         tarif = "Desole nous ne pouvons rien faire pour vous !!!" if nombr_acc else "Tarif rouge"
         print(tarif, "\n")
-
-elif (age < 25 and annee_per > 2) or (age > 25 and annee_per < 2):
+    else:
         tarif =  "Tarif orange" if not nombr_acc else "Tarif rouge" if nombr_acc == 1 else "Demande refuse"
         print(tarif, "\n")
 
-elif (age > 25) and (annee_per > 2):
-    tarif = "Tarif vert" if not nombr_acc else  "Tarif orange" if nombr_acc == 1 else "Tarif rouge" if nombr_acc == 2 else "Demande refuse"
-    print(tarif, "\n")
+else:
+    if (annee_per > 2):
+        tarif = "Tarif vert" if not nombr_acc else  "Tarif orange" if nombr_acc == 1 else "Tarif rouge" if nombr_acc == 2 else "Demande refuse"
+        print(tarif, "\n")
+    else:
+        tarif =  "Tarif orange" if not nombr_acc else "Tarif rouge" if nombr_acc == 1 else "Demande refuse"
+        print(tarif, "\n")
 
 dico_promo = {"Tarif vert":"Tarif bleu",  "Tarif orange":"Tarif vert", "Tarif rouge": "Tarif orange"}
 
